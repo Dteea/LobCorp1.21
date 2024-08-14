abc3ffcfca5991b889401aa0c6e013b57e01b2c7
DT <dt92203@gmail.com>
2024-08-14 16:19:55 -0700

    Tabs are added for risk levels, but don't appear from lowest risk to highest, maybe find out how to do that

--- src/main/java/com/dt/lobotomycorporation/creativetab/ModTab.java
+++ src/main/java/com/dt/lobotomycorporation/creativetab/ModTab.java
@@ -5,23 +5,21 @@ import com.dt.lobotomycorporation.LobotomyCorporation;
 import com.dt.lobotomycorporation.item.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
-
 import java.util.function.Supplier;
 
+//TODO: Add the risk level tabs and also weapons/armor
 public class ModTab {
-    public static final String defaultTabName = "itemGroup.lobotomycorporation.";
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LobotomyCorporation.MODID);
 
     public static final  Supplier<CreativeModeTab> ALL_ITEMS = CREATIVE_MODE_TABS.register("all_items",
             () -> CreativeModeTab.builder()
-                    .title(Component.translatable(defaultTabName + "all_items"))
+                    .title(Component.translatable("itemGroup.lobotomycorporation.all_items"))
                     .icon(() -> new ItemStack(ModItems.LCORP_LOGO.get()))
                     .displayItems((params, output) -> {
                         output.accept(ModItems.ADORATION);
@@ -32,6 +30,56 @@ public class ModTab {
                     })
                     .build());
 
+    // Risk levels
+    public static final  Supplier<CreativeModeTab> ZAYIN = CREATIVE_MODE_TABS.register("zayin",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("item.lobotomycorporation.zayin_logo"))
+                    .icon(() -> new ItemStack(ModItems.ZAYIN.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath("lobotomycorporation", "all_items"))
+                    .displayItems((params, output) -> {
+                        output.accept(ModItems.ZAYIN);
+                    })
+                    .build());
+
+    public static final  Supplier<CreativeModeTab> TETH = CREATIVE_MODE_TABS.register("teth",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("item.lobotomycorporation.teth_logo"))
+                    .icon(() -> new ItemStack(ModItems.TETH.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath("lobotomycorporation", "all_items"))
+                    .displayItems((params, output) -> {
+                        output.accept(ModItems.TETH);
+                    })
+                    .build());
+
+    public static final  Supplier<CreativeModeTab> HE = CREATIVE_MODE_TABS.register("he",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("item.lobotomycorporation.he_logo"))
+                    .icon(() -> new ItemStack(ModItems.HE.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath("lobotomycorporation", "all_items"))
+                    .displayItems((params, output) -> {
+                        output.accept(ModItems.HE);
+                    })
+                    .build());
+
+    public static final  Supplier<CreativeModeTab> WAW = CREATIVE_MODE_TABS.register("waw",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("item.lobotomycorporation.waw_logo"))
+                    .icon(() -> new ItemStack(ModItems.WAW.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath("lobotomycorporation", "all_items"))
+                    .displayItems((params, output) -> {
+                        output.accept(ModItems.WAW);
+                    })
+                    .build());
+
+    public static final  Supplier<CreativeModeTab> ALEPH = CREATIVE_MODE_TABS.register("aleph",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("item.lobotomycorporation.aleph_logo"))
+                    .icon(() -> new ItemStack(ModItems.ALEPH.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath("lobotomycorporation", "all_items"))
+                    .displayItems((params, output) -> {
+                        output.accept(ModItems.ALEPH);
+                    })
+                    .build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
--- src/main/java/com/dt/lobotomycorporation/item/ModItems.java
+++ src/main/java/com/dt/lobotomycorporation/item/ModItems.java
@@ -9,17 +9,21 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(LobotomyCorporation.MODID);
 
-    public static final DeferredItem<Item> LCORP_LOGO = ITEMS.register("lcorp_logo", () -> new Item(new Item.Properties()));
+    // Weapons
     public static final DeferredItem<Item> ADORATION = ITEMS.register("adoration", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> HEAVEN = ITEMS.register("heaven", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> LIFE_FOR_A_DAREDEVIL = ITEMS.register("life_for_a_daredevil", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> PENITENCE = ITEMS.register("penitence", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> SOLEMN_LAMENT = ITEMS.register("solemn_lament", () -> new Item(new Item.Properties()));
+
+    // Mod Icons
+    public static final DeferredItem<Item> LCORP_LOGO = ITEMS.register("lcorp_logo", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> ZAYIN = ITEMS.register("zayin_logo", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> TETH = ITEMS.register("teth_logo", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> HE = ITEMS.register("he_logo", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> WAW = ITEMS.register("waw_logo", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> ALEPH = ITEMS.register("aleph_logo", () -> new Item(new Item.Properties()));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
